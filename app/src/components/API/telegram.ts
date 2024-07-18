076ef2b85f1a81436987bc6fea00ce9c885fbd72
JupiterIT5 <geno74arhipov@gmail.com>
2024-07-18 16:05:46 +0500

    update telegram

--- app/src/components/API/telegram.ts
+++ app/src/components/API/telegram.ts
@@ -1,5 +1,3 @@
-const baseURL = "https://api.telegram.org/bot7309874600:AAHyMJWKXyz1ToTbGaDV2PdsDN6jaPV_y88/"
-
 export const sendMessage = async (data: string): Promise<void> => {
     const url = `${baseURL}sendMessage?chat_id=5186745004&text=${data}`
     await fetch(url)
